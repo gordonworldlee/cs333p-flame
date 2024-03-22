03d116981ac4ede93a152c0355ab98fbbd77a1c1
Devangi N. Parikh <dnp@cs.utexas.edu>
2024-03-22 15:00:04 -0500

    Sample Project 2, Deriving algorithms for TRMM_LLNN

---
+++ projectfiles/flameatlab/trmm_llnn_unb_var1_driver.m
@@ -0,0 +1,28 @@
+% Set the path
+% Creating character vector containing a search path that includes all the 
+% folders and subfolders in libflameatlab
+pathstoadd = genpath('../../libflameatlab');
+
+addpath(pathstoadd);
+
+%% 
+% Setting the matrix dimension
+
+m = 6;              % problem sizes
+%% 
+
+% Setting up a triangular matrix
+L = randi( [1,3], [m,m] );  % random m x m matrix
+L = tril( L );              % make the matrix lower triangular
+ 
+% Create a random matrix B
+
+B = randi( [-3,3], [m,n] );
+%% 
+% Check whether trmm_llnn_unb_var1( L, B ) computes the same as L * B
+
+if ( isequal( trmm_llnn_unb_var1( L, B ), L*B ) )
+    disp( 'All seems well' );
+else
+    disp( 'Trouble in paradise' )
+end
